5e605b94ea697c6a9fde80a76dc15efa48877b7e
hoseon <chs1025@gmail.com>
2022-12-29 08:22:24 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by HoSeon Chu on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
